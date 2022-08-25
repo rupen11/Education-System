0bc4117392013e283099dfe81b34cab4dfbc7664
rupen11 <jarsaniyarupen7984@gmail.com>
2022-08-26 01:12:16 +0530

    log

--- server/src/utils/check_by_email.ts
+++ server/src/utils/check_by_email.ts
@@ -1,8 +1,5 @@
 export const checkByEmail = async (payload: any, mongoModel: any) => {
     const matchData = await mongoModel.findOne({ email: payload });
 
-    console.log(matchData);
-
-
     return matchData ? false : true;
 }
